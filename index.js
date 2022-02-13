95aed6b4aa42fd5cfd45b850ff90c4045ed3c295
Juyo84 <juliomeme@hotmail.com>
2022-02-13 02:39:09 -0600

    Modificando la estructura del ticket

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-console.log("Pruebas");
-//Paso 1: Calcular total
-
 var total = 0;
 var monto = 0;
-var descripcion = "a";
+var descripcion = 0;
+var i = 0;
 
 var montoTxt = document.getElementById("monto");
 var descripcionTxt = document.getElementById("descripcion");
 var totalMsg = document.getElementById("totalMsg");
 
-function nuevoProducto() {
+
+function imrpimirDescripcion() {
 
     descripcion = descripcionTxt.value;
+    descripcion = String(descripcion);
+
+    if(i==0){
+        descripcionMsg.innerHTML =  descripcion;
+    }else{
+        descripcionMsg.innerHTML =  descripcionMsg.innerHTML + descripcion;
+    }
+    
+    imrpimirMonto();
+}
+
+function imrpimirMonto() {
+
     monto = montoTxt.value;
     monto = parseInt(monto);
-
+    
+    if(i==0){
+        montoMsg.innerHTML = "$" + monto;
+        i++;
+    }else{
+        montoMsg.innerHTML = montoMsg.innerHTML + "\n" + "$" + monto;        
+    }
+        
+    calcularTotal();
 }
 
 function calcularTotal() {
-    //obtener el monto del campo de texto
     
     monto = montoTxt.value;
-    monto = parseInt(monto); //resolviendo conflicto de ionic -- revisar error
+    monto = parseInt(monto);
     total = total + monto;
-    
-    totalMsg.innerHTML = "Total: $" + total;
 
+    totalMsg.innerHTML = "Total: $" + total;
 }
